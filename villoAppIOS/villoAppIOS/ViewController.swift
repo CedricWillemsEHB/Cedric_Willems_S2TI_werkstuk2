7d06aa521083be56cbe967c12bd7bf633c005acd
CedricWillems <cedric.willems@student.ehb.be>
2018-06-02 16:00:51 +0200

    project aangemaakt

---
+++ villoAppIOS/villoAppIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  villoAppIOS
+//
+//  Created by Anaïs Willems on 1/06/18.
+//  Copyright © 2018 Cedric Willems. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
