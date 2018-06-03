17e169ab9078ae944f2f2dc619667b30ebefbefc
CedricWillems <cedric.willems@student.ehb.be>
2018-06-03 03:13:49 +0200

    Data nemen van de url en tonen

--- villoAppIOS/villoAppIOS/ViewController.swift
+++ villoAppIOS/villoAppIOS/ViewController.swift
@@ -9,17 +9,66 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let urlVillo = "https://api.jcdecaux.com/vls/v1/stations?apiKey=6d5071ed0d0b3b68462ad73df43fd9e5479b03d6&contract=Bruxelles-Capitale";
+    
+    var stations = [Station]()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        stations.removeAll()
+        let url=URL(string: "https://api.jcdecaux.com/vls/v1/stations?apiKey=6d5071ed0d0b3b68462ad73df43fd9e5479b03d6&contract=Bruxelles-Capitale")
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.title = "Station List"
+        fetchJSON()
+        
+    }
+    
+    struct  Station: Decodable {
+        let name : String
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    fileprivate func fetchJSON() {
+        guard let urlJSON = URL(string: urlVillo) else { return }
+        URLSession.shared.dataTask(with: urlJSON) { (data, _, err) in
+            DispatchQueue.main.async {
+            if let err = err {
+                print("Failed to get data from url:", err)
+                return
+            }
+            guard let data = data else { return }
+            
+            do {
+                let decoder = JSONDecoder()
+                self.stations = try decoder.decode([Station].self, from: data)
+                //TODO
+                print(self.stations)
+            } catch let jsonErr {
+                print("Failed to decode:", jsonErr)
+                }
+            }
+        }.resume()
+        
+    }
+    
 
+    
+    func showNames(){
+        //looing through all the elements of the array
+        for name in stations{
+            
+            //appending the names to label
+            print(name)
+        }
+    }
 
 }
 
